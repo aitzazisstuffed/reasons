8620777c39b801ee59d61fb2c56624bb6234636a
aitzazisstuffed <aitzazimtiaz855@gmail.com>
2025-08-08 00:05:41 +0500

    Main public API

---
+++ include/reasons.h
@@ -0,0 +1,23 @@
+#ifndef REASONS_H
+#define REASONS_H
+
+// Main public API for the reasons library
+
+#include <reasons/ast.h>
+#include <reasons/collections.h>
+#include <reasons/debugger.h>
+#include <reasons/error.h>
+#include <reasons/eval.h>
+#include <reasons/explain.h>
+#include <reasons/lexer.h>
+#include <reasons/logger.h>
+#include_next <reasons/memory.h>
+#include <reasons/parser.h>
+#include <reasons/repl.h>
+#include <reasons/runtime.h>
+#include <reasons/trace.h>
+#include <reasons/tree.h>
+#include <reasons/types.h>
+#include <reasons/viz.h>
+
+#endif // REASONS_H
